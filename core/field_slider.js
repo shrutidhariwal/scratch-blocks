72a7164c3db9c87a0ef817f4f8650813671e110f
Ersin Arioglu <ersin@mit.edu>
2019-07-19 12:21:36 -0400

    Some special characters allowed now.

--- core/field_slider.js
+++ core/field_slider.js
@@ -598,8 +598,8 @@ Blockly.FieldSlider.prototype.keyboardListenerFactory = function (index) {
   return function () {
     var newArray = this.sliderStrings_.slice();
     
-    if (this.textboxes_[index].value.match(/[^a-zA-Z0-9 ]/g)) {
-      this.textboxes_[index].value = this.textboxes_[index].value.replace(/[^a-zA-Z0-9 ]/g, '');
+    if (this.textboxes_[index].value.match(/[^a-zA-Z0-9 \.\-!\?]/g)) {
+      this.textboxes_[index].value = this.textboxes_[index].value.replace(/[^a-zA-Z0-9 \.\-!\?]/g, '');
     }
     newArray[index] = this.textboxes_[index].value;
     this.setValue(this.sliders_.toString() + ';' + newArray.toString());
